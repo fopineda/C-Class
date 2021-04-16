6e04fcb03fa42d867f943ebe884d3e8f2deead98
Felipe Pineda <fpineda@live.unc.edu>
2021-04-15 20:43:57 -0400

    Started on Arrays section

---
+++ Exercise-Answers/Section6-Arrays/AccessingArray-Exercise-ans.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()				     
+{
+    // Step 1:
+        // 
+    int myIntArray[6] = {80,550,90,80,20}; 
+
+    int i;
+    for (i = 0; i < 6; ++i){
+        printf("%d\n",myIntArray[i]);
+    }
+
+}				                        
